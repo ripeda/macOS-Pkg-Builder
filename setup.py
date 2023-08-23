88ab05e638276c86c0d2fce312c1b0733c83f18b
Mykola Grymalyuk <khronokernel@icloud.com>
2023-08-23 13:48:00 -0600

    Test upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='macos_pkg_builder',
-    version='1.0.0',
+    version='0.0.1',
     author='RIPEDA',
     author_email='mykola@ripeda.com',
     license='',
@@ -10,5 +10,5 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/ripeda/Baseline-Builder-Core',
-    python_requires='>=3.6',
+    python_requires='>=3.9',
 )
